a1f8399ebf2cb1ac46f075824757d177bd40d439
Nouzbe <benjamin.amelot@gmail.com>
2020-11-01 20:22:25 +0100

    Loosen the matching of variables as Components

--- src/api/isInterpretedAsComponent.ts
+++ src/api/isInterpretedAsComponent.ts
@@ -1,12 +1,17 @@
 import { ApiFunction, ApiVariable } from "@microsoft/api-extractor-model";
+import { isFunction } from "./isFunction";
 
 /**
  * Returns whether `item` is interpreted as a Component.
  */
 export const isInterpretedAsComponent = (item: ApiFunction | ApiVariable) =>
+  // Variable explicitely typed as a Component
   item.excerptTokens.some(
     (token) =>
       token.text === "FC" ||
       token.text === "Component" ||
       token.text === "ComponentType"
-  );
+  ) ||
+  // Function returning a JSX.Element
+  (item.excerptTokens.length > 0 &&
+    item.excerptTokens[item.excerptTokens.length - 1].text === "JSX.Element");

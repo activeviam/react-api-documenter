668d524e1f0f978ce6507b31baacb18c2ea3211a
Romain Gosse <rgo@activeviam.com>
2021-07-07 13:55:32 -0400

    Improve component classification

--- src/api/isInterpretedAsComponent.ts
+++ src/api/isInterpretedAsComponent.ts
@@ -4,14 +4,27 @@ import { isFunction } from "./isFunction";
 /**
  * Returns whether `item` is interpreted as a Component.
  */
-export const isInterpretedAsComponent = (item: ApiFunction | ApiVariable) =>
+export const isInterpretedAsComponent = (item: ApiFunction | ApiVariable) => {
   // Variable explicitely typed as a Component
-  item.excerptTokens.some(
+  const isComponent = item.excerptTokens.some(
     (token) =>
       token.text === "FC" ||
       token.text === "Component" ||
-      token.text === "ComponentType"
-  ) ||
-  // Function returning a JSX.Element
-  (item.excerptTokens.length > 0 &&
-    item.excerptTokens[item.excerptTokens.length - 1].text === "JSX.Element");
+      token.text === "ComponentType" ||
+      token.text === "MemoExoticComponent" ||
+      token.text === "ForwardRefExoticComponent"
+  );
+  
+  if (isComponent) {
+    return true;
+  }
+
+  if (item.excerptTokens.length > 0) {
+    const excerptTokensExcludingDefaultProps = item.excerptTokens.filter(token => token.text.indexOf("defaultProps") === -1);
+    const lastToken = excerptTokensExcludingDefaultProps[excerptTokensExcludingDefaultProps.length - 1];
+    // Function returning a JSX.Element or ReactElement
+    return lastToken && (lastToken.text === "JSX.Element" || lastToken.text === "ReactElement");
+  }
+
+  return false;
+};

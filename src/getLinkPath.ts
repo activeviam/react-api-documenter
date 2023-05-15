f5169b6b9b5fbd3d335c6104ad3c61ed29404e2e
Nouzbe <benjamin.amelot@gmail.com>
2023-05-15 18:52:06 +0200

    Allow for baseUrl configuration

--- src/getLinkPath.ts
+++ src/getLinkPath.ts
@@ -13,22 +13,36 @@ const fileNames = [
   "variables",
 ];
 
+function getBaseURL(): string | undefined {
+  const args = process.argv;
+  const index = args.findIndex(arg => arg === "--base-url");
+  return index === 0 ? undefined : args[index + 1];
+}
+
+function getRelativePath(fileName: string, name: string, items: ApiItems) {
+  if (fileName === "types") {
+    if (isPropsTypeWithMatchingComponent(name, items)) {
+      // Found a matching component where the props are documented, hence linking to the components file.
+      return `components#${name.toLowerCase()}`;
+    } else {
+      return `types#${name.toLowerCase()}`;
+    }
+  }
+  return `${kebabCase(fileName)}#${name.toLowerCase()}`;
+}
+
 export const getLinkPath = (
   name: string,
-  items: ApiItems
+  items: ApiItems,
 ): string | undefined => {
   const fileName = fileNames.find((kind) => items[kind][name] !== undefined);
   if (!fileName) {
     return undefined;
   }
 
-  if (fileName === "types") {
-    if (isPropsTypeWithMatchingComponent(name, items)) {
-      // Found a matching component where the props are documented, hence linking to the components file.
-      return `./components#${name.toLowerCase()}`;
-    } else {
-      return `./types#${name.toLowerCase()}`;
-    }
-  }
-  return `./${kebabCase(fileName)}#${name.toLowerCase()}`;
+  const relativePath = getRelativePath(fileName, name, items);
+  
+  const baseUrl = getBaseURL();
+
+  return baseUrl ? `${baseUrl}/${relativePath}` : `./${relativePath}`;
 };

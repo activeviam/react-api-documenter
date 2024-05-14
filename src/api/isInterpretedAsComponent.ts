b18edc03afbfa3749f2e736d3c1f819fb8e3359f
oussama <ome@activeviam.com>
2024-05-14 18:01:15 +0200

    Fix components declared as variables

--- src/api/isInterpretedAsComponent.ts
+++ src/api/isInterpretedAsComponent.ts
@@ -5,7 +5,12 @@ import { isFunction } from "./isFunction";
  * Returns whether `item` is interpreted as a Component.
  */
 export const isInterpretedAsComponent = (item: ApiFunction | ApiVariable) => {
-  // Variable explicitely typed as a Component
+  const isHOC = item.name.startsWith("with");
+  if (isHOC) {
+    return false;
+  }
+
+  // Variable explicitly typed as a Component
   const isComponent = item.excerptTokens.some(
     (token) =>
       token.text === "FC" ||
@@ -14,16 +19,24 @@ export const isInterpretedAsComponent = (item: ApiFunction | ApiVariable) => {
       token.text === "MemoExoticComponent" ||
       token.text === "ForwardRefExoticComponent"
   );
-  
+
   if (isComponent) {
     return true;
   }
 
   if (item.excerptTokens.length > 0) {
-    const excerptTokensExcludingDefaultProps = item.excerptTokens.filter(token => token.text.indexOf("defaultProps") === -1);
-    const lastToken = excerptTokensExcludingDefaultProps[excerptTokensExcludingDefaultProps.length - 1];
+    const excerptTokensExcludingDefaultProps = item.excerptTokens.filter(
+      (token) => token.text.indexOf("defaultProps") === -1
+    );
+    const lastToken =
+      excerptTokensExcludingDefaultProps[
+        excerptTokensExcludingDefaultProps.length - 1
+      ];
     // Function returning a JSX.Element or ReactElement
-    return lastToken && (lastToken.text === "JSX.Element" || lastToken.text === "ReactElement");
+    return (
+      lastToken &&
+      (lastToken.text === "JSX.Element" || lastToken.text === "ReactElement")
+    );
   }
 
   return false;
